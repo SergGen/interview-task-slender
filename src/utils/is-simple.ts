d55c6a27d92deb5e1a7ee550f5681b06d3fdc661
Sergey <se.gennp@gmail.com>
2023-05-31 23:51:34 +0300

    fix: Optimise code

--- src/utils/is-simple.ts
+++ src/utils/is-simple.ts
@@ -4,12 +4,11 @@ export const isSimple = (number: number): boolean => {
   if (!isNatural(number)) {
     return false;
   }
-  let isNotSimple = true;
+
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      isNotSimple = false;
-      break;
+      return false;
     }
   }
-  return isNotSimple;
+  return true;
 };
